d837ee3456640155c325dbe6793ec9743522e955
Sophie Engle <sjengle@cs.usfca.edu>
2024-08-19 14:25:48 -0700

    Adding version check

--- src/main/java/edu/usfca/cs272/VersionCheck.java
+++ src/main/java/edu/usfca/cs272/VersionCheck.java
@@ -7,7 +7,7 @@ import java.util.SequencedCollection;
  * Designed to catch older Java installations by using new Java features.
  *
  * @author CS 272 Software Development (University of San Francisco)
- * @version Spring 2024
+ * @version Fall 2024
  */
 public class VersionCheck {
 	/**
