ba1a0376dac4e409c1c61fec65c91dc6f566e22f
Sophie Engle <sjengle@cs.usfca.edu>
2024-11-11 17:39:06 -0800

    Cleaning up archive

--- archive/resources/jdbc/SELECT.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-CONCAT(first, ' ', IFNULL(CONCAT(middle, '. '), ''), last) AS 'name',
-CONCAT(usfid, '@usfca.edu') AS 'email',
-IFNULL(githubid, '') AS 'github',
-GROUP_CONCAT(course ORDER BY course SEPARATOR ', ') AS 'courses'
-FROM faculty_names
-NATURAL LEFT OUTER JOIN faculty_github
-NATURAL LEFT OUTER JOIN faculty_courses
